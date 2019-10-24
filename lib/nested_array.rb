a41c1862e1aa914f569f9b4b368c76c982536f57
Yining Wang <yining.wang@utexas.edu>
2019-10-24 23:06:21 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -25,7 +25,7 @@ def array_literal_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  # Return the matrix's content at row and column
+  matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)

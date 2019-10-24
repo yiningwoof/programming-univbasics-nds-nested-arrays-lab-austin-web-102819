d16bb131994a0ca8c6ca4af5bfca0581cf2665ab
Yining Wang <yining.wang@utexas.edu>
2019-10-24 23:12:34 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -21,7 +21,7 @@ def assembled_matrix
 end
 
 def array_literal_matrix
-  arr = [ENNET_HOUSE.sort!, ENFIELD_TENNIS_ACADEMY.sort!]
+  arr = [ENNET_HOUSE.sort, ENFIELD_TENNIS_ACADEMY.sort]
 end
 
 def matrix_lookup(matrix, row, column)

e1e1520c4417845326f97972f3a6b6d882ab9774
Yining Wang <yining.wang@utexas.edu>
2019-10-24 23:10:45 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -29,6 +29,5 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  # Update the matrix location at row and column to have the value of new_value
-  # Return the updated matrix
+  matrxi[row][column] = new_value
 end
